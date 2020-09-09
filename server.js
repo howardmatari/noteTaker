f2db72bbeac2763171a2dd49fd60f8b1f4938de6
Matari Howard <howardmatari@gmail.com>
2020-09-08 23:44:27 -0400

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const apiRoutes=require("./routes/api");
 const htmlRoutes=require("./routes/html");
 
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
